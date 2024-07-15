62a5ee8f6af0f8b5b66e2a0b9f4c1b47a753f294
Anmol Benipal <124711038+anmolbenipal@users.noreply.github.com>
2024-07-15 14:29:42 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import EditNoteIcon from '@mui/icons-material/EditNote';
+
+
+function Header() {
+  return (
+    <header>
+      <h1>
+       
+        <EditNoteIcon />
+        DoodleDiary
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
